b9d89bc30b1ec360952f962d9a5ced72688c659a
Emanuel-Gmz <gomezema457@gmail.com>
2025-08-20 20:22:40 -0300

    Commit de las clase Conexion BD

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.example.configuracion.AdministradorConexiones;
+import org.example.dao.AutoDAO;
+import org.example.entities.Auto;
+import org.example.entities.Marca;
+
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+        System.out.println( "Hello World!" );
+        //Connection miConexion = AdministradorConexiones.obtenerConexion();
+
+        Auto auto= new Auto("CCCCCC","Blanco",2025,897,
+                Marca.Honda,"Fit");
+
+        //guardo en la BD
+        AutoDAO autoDAO = new AutoDAO();
+        autoDAO.insertarAuto(auto);
+
+        //recorreo la lista de autos
+        List<Auto> miLista = autoDAO.findAll();
+        if(!miLista.isEmpty()) {
+            for (Auto auto1 : miLista) {
+                System.out.println(auto1.toString());
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/dao/AutoDAO.java
@@ -0,0 +1,101 @@
+package org.example.dao;
+
+import org.example.configuracion.AdministradorConexiones;
+import org.example.entities.Auto;
+import org.example.entities.Marca;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoDAO {
+
+    private static Connection conn;
+
+    public void insertarAuto(Auto auto){
+
+        //1 establecer conexion a la base de datos
+        conn = AdministradorConexiones.obtenerConexion();
+        //establecer conexcion a la base de datos
+
+        //paso 2 crear String consulta SQL
+        String sql="INSERT INTO autos (idAuto,patente,color,anio,kilometraje,marca,modelo) " +
+                "VALUES (" + auto.getIdAuto() + "," +
+                "'" + auto.getPatente() + "'," +
+                "'" + auto.getColor() + "'," +
+                + auto.getAnio()+ "," +
+                + auto.getKilometraje() + "," +
+                "'" + auto.getMarca() + "'," +
+                "'" + auto.getModelo() + "')" ;
+
+        //paso 3 crear instruccion
+        Statement st=null;
+        try {
+            st= conn.createStatement();
+
+            //paso 4 ejecutar instruccion
+            st.execute(sql);
+
+            //paso 5 cerrar conexion
+            st.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    public List<Auto> findAll(){
+
+        //1 conectar
+        conn = AdministradorConexiones.obtenerConexion();
+
+        //2 crear consulta SQL
+        String sql= "SELECT * FROM autos order by patente";
+
+        //3 crear statement y resultset
+        Statement st= null;
+        ResultSet rs= null;
+
+        List<Auto> listaAutos = new java.util.ArrayList<>();
+        try {
+            //paso 3 crear instruccion
+            st=conn.createStatement();
+            //paso 4 ejecutar consulta y guarda el resultado en resultset
+            rs = st.executeQuery(sql);
+
+
+
+            //paso 5 recorrer el resultset y guardar los autos en una lista
+            while(rs.next()){
+                Auto auto = new Auto();
+                auto.setIdAuto(rs.getInt("idAuto"));
+                auto.setAnio(rs.getInt("anio"));
+                auto.setPatente(rs.getString("patente"));
+                auto.setColor(rs.getString("color"));
+                auto.setKilometraje(rs.getInt("kilometraje"));
+                auto.setMarca(Marca.valueOf(rs.getString("marca")));
+                auto.setModelo(rs.getString("modelo"));
+
+                listaAutos.add(auto);
+            }
+            //paso 6 cerrar el resultset y statement
+            rs.close();
+            st.close();
+            conn.close();
+
+
+        } catch (SQLException e) {
+            System.out.println("Error al crear el statement");
+            throw new RuntimeException(e);
+        }
+
+
+        return listaAutos;
+    }
+
+}
